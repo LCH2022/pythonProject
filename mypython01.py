dc788aa897c6c2f1cec19f40df4a6020bcabcd90
LCH2022 <thomasleeone7@gmail.com>
2022-04-10 16:40:32 +0800

    commit20

--- mypython01.py
+++ mypython01.py
@@ -1,5 +1,8 @@
 print("helloworld!")
 print("helloworld2!")
-print("helloworld14!")
-print("helloworld15!")
-print("helloworld16!")
+print("helloworld3!")
+
+print("helloworld11!")
+print("helloworld12!")
+
+print("helloworld18!")
